1b40f999ec43aed9f887d1f28a69912003f99ce5
hyhlinux <2285020853@qq.com>
2016-12-25 20:55:47 +0800

    [ALL][Modular][function]
    backup
    plan:
    Type:
    Test:

---
+++ main/006main_appinfo_ok.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"strings"
+	"user/common"
+)
+
+type AppInfo struct {
+	Developer    string `json:"developer"`
+	Detail_url   string `json:"detail_url"`
+	Icon         string `json:"icon"`
+	Name         string `json:"name"`
+	Package_name string `json:"package_name"`
+	Review_stars string `json:"review_stars"`
+}
+
+func main() {
+	// TestJson(nil)
+	hc := common.NewHttpClient("https://p.xgj.me:27035")
+	if hc == nil {
+		log.Println("NewHttpClient..err")
+		return
+	}
+
+	//search
+	applist, err := search("facebook", hc)
+	if err != nil {
+		panic(err)
+		return
+	}
+	for index, app := range applist {
+		jsonBuf, err := json.Marshal(app)
+		if err != nil {
+			panic(err)
+			return
+		}
+		fmt.Println("第", index, "个结果是:", string(jsonBuf))
+	}
+}
+
+/////////////////////////
+//查询页面接口：
+//例如	:
+//		search("facebook") int {}
+//return:
+//		查询结果数量 & img_src_list
+//func search(query_app string, hc *common.HttpClient) ([]byte, error) {
+func search(query_app string, hc *common.HttpClient) ([]*AppInfo, error) {
+
+	if len(query_app) <= 0 {
+		return nil, nil
+	}
+	// url_base := "https://play.google.com/store/search?q=facebook"
+	url_base := "https://play.google.com/store/search"
+	url := fmt.Sprintf("%s?q=%s", url_base, query_app)
+	log.Println(url)
+	req, e := http.NewRequest(
+		"GET",
+		url,
+		nil,
+	)
+
+	resp, e := hc.Do(req)
+	if e != nil {
+		panic(e)
+		return nil, e
+	}
+
+	// Create and fill the document, defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		panic(err)
+		return nil, nil
+	}
+	sum := 0
+	resList := []*AppInfo{}
+	doc.Find(".card-content").Each(
+		func(i int, contentSelection *goquery.Selection) {
+			app_deatil_url, _ := contentSelection.ChildrenFiltered(".card-click-target").Attr("href")
+			if !strings.HasPrefix(app_deatil_url, "https:") {
+				app_deatil_url = "https://play.google.com" + app_deatil_url
+			}
+
+			app_icon, _ := contentSelection.Find(".cover-image").Attr("src")
+			if !strings.HasPrefix(app_icon, "https:") {
+				app_icon = "https:" + app_icon
+			}
+
+			app_name, _ := contentSelection.Find(".title").Attr("title")
+			app_developer, _ := contentSelection.Find(".subtitle").Attr("title")
+			app_package_arr := strings.Split(app_deatil_url, "?id=")
+
+			app_package_name := ""
+			if len(app_package_arr) == 2 {
+				app_package_name = app_package_arr[1]
+			} else {
+				app_package_name = "not found id"
+			}
+			app_stars, _ := contentSelection.Find(".tiny-star").Attr("aria-label")
+
+			resList = append(resList, &AppInfo{
+				Name:         app_name,
+				Icon:         app_icon,
+				Developer:    app_developer,
+				Review_stars: app_stars,
+				Detail_url:   app_deatil_url,
+				Package_name: app_package_name,
+			})
+			sum += 1
+		})
+	log.Println("sum:", sum)
+	//jsonBuf, e := json.Marshal(&resList)
+	//fmt.Println(string(jsonBuf), e)
+	//return jsonBuf, nil
+	return resList, nil
+}
